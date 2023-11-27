198e52cb0229dd1f17755bbff612c2ca66eb8794
riacrdohorie <ricardohorie07@gmail.com>
2023-11-27 20:11:55 -0300

    atualizando a classe destinoview

--- src/Destino/view/DestinoView.java
+++ src/Destino/view/DestinoView.java
@@ -7,22 +7,26 @@ import javax.swing.JOptionPane;
 public class DestinoView {
     // Método para exibir detalhes sobre o destino
     public void mostrarDetalhesDestino(Destino destino) {
-        StringBuilder detalhes = new StringBuilder("Detalhes do Destino:\n");
-        detalhes.append("Nome: ").append(destino.getNome()).append("\n");
-        detalhes.append("Descrição: ").append(destino.getDescricao()).append("\n");
-        detalhes.append("Categoria: ").append(destino.getCategoria()).append("\n");
-        detalhes.append("País: ").append(destino.getPais()).append("\n");
-        detalhes.append("Cidade: ").append(destino.getCidade()).append("\n\n");
+        try {
+            StringBuilder detalhes = new StringBuilder("Detalhes do Destino:\n");
+            detalhes.append("Nome: ").append(destino.getNome()).append("\n");
+            detalhes.append("Descrição: ").append(destino.getDescricao()).append("\n");
+            detalhes.append("Categoria: ").append(destino.getCategoria()).append("\n");
+            detalhes.append("País: ").append(destino.getPais()).append("\n");
+            detalhes.append("Cidade: ").append(destino.getCidade()).append("\n\n");
 
-        // Exibindo informações sobre hotéis associados ao destino
-        detalhes.append("Hotéis Associados:\n");
-        destino.getHoteis().forEach(hotel -> detalhes.append("- ").append(hotel.getNome()).append("\n"));
+            // Exibindo informações sobre hotéis associados ao destino
+            detalhes.append("Hotéis Associados:\n");
+            destino.getHoteis().forEach(hotel -> detalhes.append("- ").append(hotel.getNome()).append("\n"));
 
-        // Exibindo informações sobre transportes associados ao destino
-        detalhes.append("\nTransportes Associados:\n");
-        destino.getTransportes().forEach(transporte -> detalhes.append("- ").append(transporte.getNome()).append("\n"));
+            // Exibindo informações sobre transportes associados ao destino
+            detalhes.append("\nTransportes Associados:\n");
+            destino.getTransportes().forEach(transporte -> detalhes.append("- ").append(transporte.getNome()).append("\n"));
 
-        // Exibir as informações usando JOptionPane
-        JOptionPane.showMessageDialog(null, detalhes.toString(), "Detalhes do Destino", JOptionPane.INFORMATION_MESSAGE);
+            // Exibir as informações usando JOptionPane
+            JOptionPane.showMessageDialog(null, detalhes.toString(), "Detalhes do Destino", JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Ocorreu um erro ao exibir os detalhes do destino: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
