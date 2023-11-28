595dfaaa7e6f363f4c3fe4c56b013a4b8574e0c4
riacrdohorie <ricardohorie07@gmail.com>
2023-11-27 22:16:55 -0300

    atualizando a classe pacoteview

--- src/Pacotes/view/PacoteView.java
+++ src/Pacotes/view/PacoteView.java
@@ -15,7 +15,8 @@ public class PacoteView<T extends Pacote> {
 
     // Método para adicionar um novo pacote de cultura
 	public PacoteCultura adicionarPacoteCultura(List<Destino> Destinos, List<Guia> Guias, List<Hotel> Hoteis, List<Transporte> Transportes, List<Passeio_Cultura> Cultura) {
-        // Listas para armazenar informações do pacote de cultura
+		try {
+		// Listas para armazenar informações do pacote de cultura
         List<Destino> destinos = new ArrayList<>();
         List<Hotel> hoteis = new ArrayList<>();
         List<Transporte> transportes = new ArrayList<>();
@@ -51,10 +52,17 @@ public class PacoteView<T extends Pacote> {
         PacoteCultura pacoteCultura = new PacoteCultura(destinos, hoteis, transportes, agendamento, guias, culturas);
         JOptionPane.showMessageDialog(null, "Pacote de Cultura adicionado com sucesso!");
         return pacoteCultura;
+	 
+	} catch (Exception e) {
+	        JOptionPane.showMessageDialog(null, "Erro ao processar entrada do usuário: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+	        // Lide com o erro de acordo com a lógica do seu programa.
+	    }
+	    return null; // Ou retorne um valor padrão, dependendo da lógica do seu programa em caso de erro.
     }
 
     // Método para adicionar um novo pacote de aventura
 	public PacoteRelax adicionarPacoteRelax(List<Destino> destinos, List<Guia> guias, List<Hotel> hoteis, List<Transporte> transportes, List<Passeio_Relax> relax) {
+		try {
 	    List<Destino> destinosRelax = new ArrayList<>();
 	    List<Hotel> hoteisRelax = new ArrayList<>();
 	    List<Transporte> transportesRelax = new ArrayList<>();
@@ -83,10 +91,18 @@ public class PacoteView<T extends Pacote> {
 	    PacoteRelax pacoteRelax = new PacoteRelax(destinosRelax, hoteisRelax, transportesRelax, agendamento, relaxacoes);
 	    JOptionPane.showMessageDialog(null, "Pacote de Relaxamento adicionado com sucesso!");
 	    return pacoteRelax;
+	    
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Erro ao processar entrada do usuário: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+			// Lide com o erro de acordo com a lógica do seu programa.
+		}
+		return null; // Ou retorne um valor padrão, dependendo da lógica do seu programa em caso de erro.
 	}
 
+
 	public PacoteAventura adicionarPacoteAventura(List<Destino> destinos, List<Guia> guias, List<Hotel> hoteis, List<Transporte> transportes, List<Passeio_Aventura> aventuras) {
-	    List<Destino> destinosAventura = new ArrayList<>();
+	    try {
+		List<Destino> destinosAventura = new ArrayList<>();
 	    List<Hotel> hoteisAventura = new ArrayList<>();
 	    List<Transporte> transportesAventura = new ArrayList<>();
 	    List<Passeio_Aventura> aventurasList = new ArrayList<>();
@@ -114,112 +130,148 @@ public class PacoteView<T extends Pacote> {
 	    PacoteAventura pacoteAventura = new PacoteAventura(destinosAventura, hoteisAventura, transportesAventura, agendamento, aventurasList);
 	    JOptionPane.showMessageDialog(null, "Pacote de Aventura adicionado com sucesso!");
 	    return pacoteAventura;
+	    
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Erro ao processar entrada do usuário: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+			// Lide com o erro de acordo com a lógica do seu programa.
+		}
+		return null; // Ou retorne um valor padrão, dependendo da lógica do seu programa em caso de erro.
 	}
+	
 
     // Método para listar informações de um pacote
     public void listarInformacoesPacote(T pacote) {
-        StringBuilder informacoes = new StringBuilder("Informações do Pacote:\n");
+    	try {
+            StringBuilder informacoes = new StringBuilder("Informações do Pacote:\n");
 
-        informacoes.append("Destinos:\n");
-        for (Object destino : pacote.getDestinos()) {
-            if (destino instanceof Destino) {
-                informacoes.append(((Destino) destino).getNome()).append("\n");
+            informacoes.append("Destinos:\n");
+            for (Object destino : pacote.getDestinos()) {
+                if (destino instanceof Destino) {
+                    informacoes.append(((Destino) destino).getNome()).append("\n");
+                }
             }
-        }
 
-        informacoes.append("Hoteis:\n");
-        for (Object hotel : pacote.getHoteis()) {
-            if (hotel instanceof Hotel) {
-                informacoes.append(((Hotel) hotel).getNome()).append("\n");
+            informacoes.append("Hotéis:\n");
+            for (Object hotel : pacote.getHoteis()) {
+                if (hotel instanceof Hotel) {
+                    informacoes.append(((Hotel) hotel).getNome()).append("\n");
+                }
             }
-        }
 
-        informacoes.append("Transportes:\n");
-        for (Object transporte : pacote.getTransportes()) {
-            if (transporte instanceof Transporte) {
-                informacoes.append(((Transporte) transporte).getNome()).append("\n");
+            informacoes.append("Transportes:\n");
+            for (Object transporte : pacote.getTransportes()) {
+                if (transporte instanceof Transporte) {
+                    informacoes.append(((Transporte) transporte).getNome()).append("\n");
+                }
             }
-        }
 
-        informacoes.append("Passeios:\n");
-        for (Object passeio : pacote.getPasseios()) {
-            if (passeio instanceof Passeio) {
-                informacoes.append(((Passeio) passeio).getNome()).append("\n");
+            informacoes.append("Passeios:\n");
+            for (Object passeio : pacote.getPasseios()) {
+                if (passeio instanceof Passeio) {
+                    informacoes.append(((Passeio) passeio).getNome()).append("\n");
+                }
             }
+
+            informacoes.append("Agendamento:\n")
+                    .append("Ida: ").append(pacote.getAgendamento().getDataIda()).append("\n")
+                    .append("Volta: ").append(pacote.getAgendamento().getDataVolta()).append("\n")
+                    .append("Informações: ").append(pacote.getAgendamento().getInformacoes()).append("\n")
+                    .append("Checkin: ").append(pacote.getAgendamento().getCheckin()).append("\n")
+                    .append("Checkout: ").append(pacote.getAgendamento().getCheckout()).append("\n");
+
+            JOptionPane.showMessageDialog(null, informacoes.toString(), "Informações do Pacote", JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Ocorreu um erro ao listar as informações do pacote: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
         }
-
-
-        informacoes.append("Agendamento:\n")
-                   .append("Ida: ").append(pacote.getAgendamento().getDataIda()).append("\n")
-                   .append("Volta: ").append(pacote.getAgendamento().getDataVolta()).append("\n")
-                   .append("Informações: ").append(pacote.getAgendamento().getInformacoes()).append("\n")
-                   .append("Checkin: ").append(pacote.getAgendamento().getCheckin()).append("\n")
-                   .append("Checkout: ").append(pacote.getAgendamento().getCheckout()).append("\n");
-
-        JOptionPane.showMessageDialog(null, informacoes.toString(), "Informações do Pacote", JOptionPane.INFORMATION_MESSAGE);
     }
     
     public void adicionarDestinoInterativo(List<Destino> destinosDisponiveis, List<Destino> destinosParaAdicionar) {
         // Mostra os destinos disponíveis e permite ao usuário decidir se deseja adicionar cada um
         for (Destino destino : destinosDisponiveis) {
-            int resposta = JOptionPane.showConfirmDialog(null, "Adicionar destino: " + destino.getNome() + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
-            if (resposta == JOptionPane.YES_OPTION) {
-                // Adiciona o destino à lista específica se o usuário escolher sim
-                destinosParaAdicionar.add(destino);
+            try {
+                int resposta = JOptionPane.showConfirmDialog(null, "Adicionar destino: " + destino.getNome() + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
+                if (resposta == JOptionPane.YES_OPTION) {
+                    // Adiciona o destino à lista específica se o usuário escolher sim
+                    destinosParaAdicionar.add(destino);
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro ao adicionar destino: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
             }
         }
     }
+   
     public <T extends Passeio> void adicionarPasseioInterativo(List<T> passeiosDisponiveis, List<T> passeiosParaAdicionar) {
         for (T passeio : passeiosDisponiveis) {
-            int resposta = JOptionPane.showConfirmDialog(null, "Adicionar passeio: " + passeio.getNome() + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
-            if (resposta == JOptionPane.YES_OPTION) {
-                passeiosParaAdicionar.add(passeio);
+            try {
+                int resposta = JOptionPane.showConfirmDialog(null, "Adicionar passeio: " + passeio.getNome() + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
+                if (resposta == JOptionPane.YES_OPTION) {
+                    passeiosParaAdicionar.add(passeio);
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro ao adicionar passeio: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
             }
         }
     }
 
 
-    // Método para adicionar guias interativamente a uma lista específica
+ // Método para adicionar guias interativamente a uma lista específica
     public void adicionarGuiaInterativo(List<Guia> guiasDisponiveis, List<Guia> guiasParaAdicionar) {
         for (Guia guia : guiasDisponiveis) {
-            int resposta = JOptionPane.showConfirmDialog(null, "Adicionar guia: " + guia.getEspecialidade() + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
-            if (resposta == JOptionPane.YES_OPTION) {
-                guiasParaAdicionar.add(guia);
+            try {
+                int resposta = JOptionPane.showConfirmDialog(null, "Adicionar guia: " + guia.getEspecialidade() + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
+                if (resposta == JOptionPane.YES_OPTION) {
+                    guiasParaAdicionar.add(guia);
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro ao adicionar guia: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
             }
         }
     }
-
-    // Método para adicionar transportes interativamente a uma lista específica
+    
+ // Método para adicionar transportes interativamente a uma lista específica
     public void adicionarTransporteInterativo(List<Transporte> transportesDisponiveis, List<Transporte> transportesParaAdicionar) {
         for (Transporte transporte : transportesDisponiveis) {
-            int resposta = JOptionPane.showConfirmDialog(null, "Adicionar transporte: " + transporte.getNome() + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
-            if (resposta == JOptionPane.YES_OPTION) {
-                transportesParaAdicionar.add(transporte);
+            try {
+                int resposta = JOptionPane.showConfirmDialog(null, "Adicionar transporte: " + transporte.getNome() + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
+                if (resposta == JOptionPane.YES_OPTION) {
+                    transportesParaAdicionar.add(transporte);
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro ao adicionar transporte: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
             }
         }
     }
 
-    // Método para adicionar hotéis interativamente a uma lista específica
+ // Método para adicionar hotéis interativamente a uma lista específica
     public void adicionarHotelInterativo(List<Hotel> hoteisDisponiveis, List<Hotel> hoteisParaAdicionar) {
         for (Hotel hotel : hoteisDisponiveis) {
-            int resposta = JOptionPane.showConfirmDialog(null, "Adicionar hotel: " + hotel.getNome() + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
-            if (resposta == JOptionPane.YES_OPTION) {
-                hoteisParaAdicionar.add(hotel);
+            try {
+                int resposta = JOptionPane.showConfirmDialog(null, "Adicionar hotel: " + hotel.getNome() + "?", "Confirmação", JOptionPane.YES_NO_OPTION);
+                if (resposta == JOptionPane.YES_OPTION) {
+                    hoteisParaAdicionar.add(hotel);
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro ao adicionar hotel: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);             
             }
         }
     }
     
     public Agendamento adicionarAgendamentoInterativo() {
-    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        LocalDateTime ida = LocalDateTime.parse(JOptionPane.showInputDialog("Digite a data de início (AAAA-MM-DD HH:mm:ss):"), formatter);
-        LocalDateTime volta = LocalDateTime.parse(JOptionPane.showInputDialog("Digite a data de término (AAAA-MM-DD HH:mm:ss):"), formatter);
-        String informacoes = JOptionPane.showInputDialog("Digite informações sobre o agendamento:");
-        LocalDateTime checkin = LocalDateTime.parse(JOptionPane.showInputDialog("Digite a data de check-in (AAAA-MM-DD HH:mm:ss):"), formatter);
-        LocalDateTime checkout = LocalDateTime.parse(JOptionPane.showInputDialog("Digite a data de check-out (AAAA-MM-DD HH:mm:ss):"), formatter);
+        try {
+            LocalDateTime ida = LocalDateTime.parse(JOptionPane.showInputDialog("Digite a data de início (AAAA-MM-DD HH:mm:ss):"), formatter);
+            LocalDateTime volta = LocalDateTime.parse(JOptionPane.showInputDialog("Digite a data de término (AAAA-MM-DD HH:mm:ss):"), formatter);
+            String informacoes = JOptionPane.showInputDialog("Digite informações sobre o agendamento:");
+            LocalDateTime checkin = LocalDateTime.parse(JOptionPane.showInputDialog("Digite a data de check-in (AAAA-MM-DD HH:mm:ss):"), formatter);
+            LocalDateTime checkout = LocalDateTime.parse(JOptionPane.showInputDialog("Digite a data de check-out (AAAA-MM-DD HH:mm:ss):"), formatter);
 
-        Agendamento agendamento = new Agendamento(ida, volta, informacoes, checkin, checkout);
-        return agendamento;
+            return new Agendamento(ida, volta, informacoes, checkin, checkout);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao processar entrada do usuário: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+            return null;
+        }
     }
     
 }
+
